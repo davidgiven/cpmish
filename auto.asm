8b0da1628be6f210a2cc5d584c74d5efb83fff14
David Given <dg@cowlark.com>
2018-11-20 21:06:58 +0100

    The boot loader now seems to work and will load the supervisor into RAM.

--- auto.asm
+++ auto.asm
@@ -1,16 +1,90 @@
-textout:     equ 0xb81e
-diskservice: equ 0xba5e
-kmwaitkbd:   equ 0xb80f
+CONTROL:        equ 0x30
+FD_POWER:       equ 0x20
+FD_ST:          equ 0xe0
+FD_DT:          equ 0xe1
 
 org 0x4000
 
-main:
-    ld c, 0x30
-    call diskservice
-loop:
-    ld hl, .str.hello_world
-    call textout
-    call kmwaitkbd
-    ret
+; On entry, 0x0000 contains bank 0x40 and 0x4000 contains (usually) 0x47. This
+; means we can write to 0x0000 without stepping on ourselves.
 
-.str.hello_world: db "Hello, world! ", 0
+main:
+    ; Interrupts *off*! We want to kill the NC200 OS ASAP.
+
+    di
+
+    ; Now, load the entire first track to 0x0000. The 765 has just loaded
+    ; this boot block so we know it's correctly seeked and spun up. We
+    ; just need to issue the command and we're ready.
+
+    ; Send the read command to the 765.
+
+    ld hl, .data.readtrack
+    ld b, .data.readtrack.end - .data.readtrack
+    ld c, FD_DT
+cmd_loop:
+	in a, (FD_ST)
+	rla							; RQM...
+	jr nc, cmd_loop             ; ...low, keep waiting
+    outi                        ; port(C) = (HL++), B++
+    jr nz, cmd_loop
+
+    ; Now read bytes from the 765 until we're done.
+
+	ld hl, 0                    ; destination address
+	; ld c, FD_DT               ; still in C from previously
+read_loop:
+	in a, (FD_ST)
+	rla							; RQM...
+	jr nc, read_loop      		; ...low, keep waiting
+	rla							; DIO (ignore)
+	rla							; EXM...
+	jr nc, read_finished		; ...low, transfer complete
+	ini							; (HL++) = port(C), B--
+    jr read_loop
+read_finished:
+
+    ; We've finished a command, so nudge the FDC TC line to tell it
+    ; we're done.
+
+	ld a, 0x83
+	out (FD_POWER), a
+	dec a
+	out (FD_POWER), a
+
+    ; Read and then discard the status.
+
+read_status_loop:
+	in a, (FD_ST)
+	rla 						; RQM...
+	jr nc, read_status_loop 	; ...low, keep waiting 
+	rla							; DIO...
+    jr nc, finished             ; ...low, no more data
+    in a, (FD_DT)
+	jr read_status_loop
+
+    ; We're done.
+
+finished:
+    ; Turn the FDD motor off.
+
+    ld a, 0xb8
+    out (CONTROL), a
+
+    ; And jump to the newly loaded code.
+
+    jp 0
+
+; The baked command to read a track.
+
+.data.readtrack:
+    db 0xc2     ; 0: READ TRACK MULTITRACK
+    db 0        ; 1: physical head 0, drive 0
+    db 0        ; 2: track 0
+    db 0        ; 3: logical head 0
+    db 0        ; 4: unused
+    db 2        ; 5: bytes per sector: 512
+    db 9        ; 6: last sector for one track (*inclusive*)
+    db 27       ; 7: gap 3 length (27 is standard for 3.5" drives)
+    db 0        ; 8: sector length (unused)
+.data.readtrack.end
