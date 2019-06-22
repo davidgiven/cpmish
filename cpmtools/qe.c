e2d9949985f15d44819e9adc3437d46410821cdc
David Given <dg@cowlark.com>
2019-06-22 20:18:36 +0200

    Now simplified version seems to work with navigation controls.

--- cpmtools/qe.c
+++ cpmtools/qe.c
@@ -10,87 +10,24 @@
 #define WIDTH 80
 #define HEIGHT 23
 
-typedef uint8_t uc;
+uint8_t screenx, screeny;
+uint8_t status_line_length;
 
-uc physical_screen[HEIGHT][WIDTH];
-uc logical_screen[HEIGHT][WIDTH];
-#define logical_screen_start (logical_screen[0])
-#define logical_screen_end (logical_screen[HEIGHT])
-uc screenx, screeny;
-uc* screenptr;
-uc status_line_length;
+uint8_t* buffer_start;
+uint8_t* gap_start;
+uint8_t* gap_end;
+uint8_t* buffer_end;
 
-uc* buffer_start;
-uc* gap_start;
-uc* gap_end;
-uc* buffer_end;
-
-uint16_t first_line;
-uint16_t cursor;
-uc cursorx, cursory;
-uint16_t current_line;
-uc current_line_y;
+uint8_t* first_line; /* <= gap_start */
+uint8_t* current_line; /* <= gap_start */
+uint8_t* old_current_line;
+unsigned current_line_y;
 
 /* ======================================================================= */
-/*                                SCREEN REFRESH                           */
+/*                                SCREEN DRAWING                           */
 /* ======================================================================= */
 
-void con_goto(uc x, uc y)
-{
-	screenx = x;
-	screeny = y;
-	screenptr = &logical_screen[screeny][screenx];
-}
-
-void con_clear(void)
-{
-	memset(logical_screen, ' ', sizeof(logical_screen));
-	con_goto(0, 0);
-}
-
-void con_init(void)
-{
-	con_clear();
-	memset(physical_screen, ' ', sizeof(physical_screen));
-	bios_conout(26);
-}
-
-void con_newline(void)
-{
-	screenx = 0;
-	screeny++;
-	con_goto(screenx, screeny);
-}
-
-void con_putc(uc c)
-{
-	if (screeny >= HEIGHT)
-		return;
-
-	if (c < 32)
-	{
-		con_putc('^');
-		c += '@';
-	}
-
-	*screenptr++ = c;
-	screenx++;
-	if (screenx == WIDTH)
-		con_newline();
-}
-
-void con_puts(const char* s)
-{
-	for (;;)
-	{
-		uc c = *s++;
-		if (!c)
-			break;
-		con_putc(c);
-	}
-}
-
-void scr_goto(uc x, uc y)
+void con_goto(uint8_t x, uint8_t y)
 {
 	if (!x && !y)
 		bios_conout(30);
@@ -101,58 +38,96 @@ void scr_goto(uc x, uc y)
 		bios_conout(y + ' ');
 		bios_conout(x + ' ');
 	}
+	screenx = x;
+	screeny = y;
 }
 
-void con_refresh(void)
+void con_clear(void)
 {
-	static uc* lptr;
-	static uc* pptr;
+	bios_conout(26);
+	screenx = screeny = 0;
+}
 
-	lptr = logical_screen[0];
-	pptr = physical_screen[0];
-	while (lptr != logical_screen_end)
+/* Leaves cursor at the beginning of the *next* line. */
+void con_clear_to_eol(void)
+{
+	if (screeny >= HEIGHT)
+		return;
+
+	while (screenx != WIDTH)
 	{
-		static uint16_t offset;
-		static uc x, y;
-
-		for (;;)
-		{
-			if (*lptr != *pptr)
-				break;
-			lptr++;
-			pptr++;
-			if (lptr == logical_screen_end)
-				goto done;
-		}
-
-		offset = lptr - logical_screen[0];
-		y = offset / WIDTH;
-		x = offset % WIDTH;
-
-		scr_goto(x, y);
-		for (;;)
-		{
-			uc c = *lptr;
-			if (c == *pptr)
-				break;
-			lptr++;
-			*pptr++ = c;
-
-			bios_conout(c);
-		}
+		bios_conout(' ');
+		screenx++;
 	}
-done:;
+	screenx = 0;
+	screeny++;
+}
 
-	scr_goto(screenx, screeny);
+void con_clear_to_eos(void)
+{
+	while (screeny < HEIGHT)
+		con_clear_to_eol();
+}
+
+void con_newline(void)
+{
+	if (screeny >= HEIGHT)
+		return;
+
+	bios_conout('\n');
+	screenx = 0;
+	screeny++;
+}
+
+void con_putc(uint8_t c)
+{
+	if (screeny >= HEIGHT)
+		return;
+
+	if (c < 32)
+	{
+		con_putc('^');
+		c += '@';
+	}
+
+	bios_conout(c);
+	screenx++;
+	if (screenx == WIDTH)
+	{
+		screenx = 0;
+		screeny++;
+	}
+}
+
+void con_puts(const char* s)
+{
+	for (;;)
+	{
+		char c = *s++;
+		if (!c)
+			break;
+		con_putc((uint8_t) c);
+	}
+}
+
+void con_puti(long i)
+{
+	itoa(i, (char*)cpm_default_dma, 10);
+	con_puts((char*) cpm_default_dma);
 }
 
 void set_status_line(const char* message)
 {
-	uc length = 0;
-	scr_goto(0, HEIGHT);
+	uint16_t length = 0;
+
+	bios_conout(27);
+	bios_conout('=');
+	bios_conout(HEIGHT + ' ');
+	bios_conout(0 + ' ');
+
 	for (;;)
 	{
-		uc c = *message++;
+		uint16_t c = *message++;
 		if (!c)
 			break;
 		bios_conout(c);
@@ -164,78 +139,70 @@ void set_status_line(const char* message)
 		length++;
 	}
 	status_line_length = length;
-	scr_goto(screenx, screeny);
+	con_goto(screenx, screeny);
 }
 
 /* ======================================================================= */
 /*                              BUFFER MANAGEMENT                          */
 /* ======================================================================= */
 
-uc* ptr(uint16_t index)
-{
-	uc* p = buffer_start + index;
-	if (p < gap_start)
-		return p;
-	return gap_end + (gap_start - buffer_start);
-}
-
-uint16_t pos(const uc* ptr)
-{
-	if (ptr < gap_start)
-		return ptr - buffer_start;
-	return (gap_start - buffer_start) + (ptr - gap_end);
-}
-
 void new_file(void)
 {
 	gap_start = buffer_start;
 	gap_end = buffer_end;
 
-	first_line = 0;
-	cursor = 0;
-	current_line = 0;
-	current_line_y = 0;
+	first_line = current_line = buffer_start;
 }
 	
-void render_screen(void)
+uint16_t compute_length(const uint8_t* inp, const uint8_t* endp, const uint8_t** nextp)
 {
-	static uc* inp;
-	static uc* cursorp;
-	static uc* currentlinep;
-	static uc xo;
+	uint16_t xo = 0;
+	uint16_t c;
 
-	inp = ptr(first_line);
-	cursorp = ptr(cursor);
-	currentlinep = ptr(current_line);
-	xo = 0;
+	for (;;)
+	{
+		if (inp == endp)
+			break;
+		if (inp == gap_start)
+			inp = gap_end;
 
-	con_clear();
+		c = *inp++;
+		if (c == '\n')
+			break;
+		if (c == '\t')
+			xo = (xo + 8) & ~7;
+		else if (c < 32)
+			xo += 2;
+		else
+			xo++;
+	}
+
+	if (nextp)
+		*nextp = inp;
+	return xo;
+}
+
+const uint8_t* draw_line(const uint8_t* inp)
+{
+	uint16_t xo = 0;
+	uint16_t c;
+
 	while (screeny != HEIGHT)
 	{
-		static uc c;
-
 		if (inp == gap_start)
 			inp = gap_end;
 		if (inp == buffer_end)
 		{
-			con_newline();
 			con_puts("<<EOF>>");
+			con_clear_to_eol();
 			break;
 		}
 
-		if (inp == cursorp)
-		{
-			cursorx = screenx;
-			cursory = screeny;
-		}
 		c = *inp++;
 		if (c == '\n')
 		{
-			con_newline();
-			xo = 0;
-
-			if (inp == currentlinep)
-				current_line_y = screeny;
+			con_clear_to_eol();
+			break;
 		}
 		else if (c == '\t')
 		{
@@ -253,23 +220,103 @@ void render_screen(void)
 		}
 	}
 
-	con_goto(cursorx, cursory);
-	con_refresh();
+	return inp;
+}
+
+/* inp <= gap_start */
+void render_screen(const uint8_t* inp)
+{
+	while (screeny != HEIGHT)
+	{
+		if (inp == current_line)
+			current_line_y = screeny;
+		inp = draw_line(inp);
+		if (inp == buffer_end)
+		{
+			con_clear_to_eos();
+			break;
+		}
+	}
+}
+
+void recompute_screen_position(void)
+{
+	const uint8_t* inp = first_line;
+
+	current_line_y = 0;
+	while (current_line_y != HEIGHT)
+	{
+		uint16_t length;
+
+		if (inp == current_line)
+			break;
+
+		length = compute_length(inp, buffer_end, &inp);
+		current_line_y += (length / WIDTH) + 1;
+	}
 }
 
 /* ======================================================================= */
 /*                            EDITOR OPERATIONS                            */
 /* ======================================================================= */
 
-void cursor_right(void)
+void cursor_left(void)
 {
-	if (cursor != pos(buffer_end))
-		cursor++;
+	if ((gap_start != buffer_start) && (gap_start[-1] != '\n'))
+		*--gap_end = *--gap_start;
 }
 
-const char editor_keys[] = "l";
+void cursor_right(void)
+{
+	if ((gap_end != buffer_end) && (gap_end[0] != '\n'))
+		*gap_start++ = *gap_end++;
+}
+
+void cursor_down(void)
+{
+	uint16_t offset = gap_start - current_line;
+	while ((gap_end != buffer_end) && (gap_end[0] != '\n'))
+		*gap_start++ = *gap_end++;
+	if (gap_end == buffer_end)
+		return;
+		
+	*gap_start++ = *gap_end++;
+	current_line = gap_start;
+	while (offset--)
+		cursor_right();
+}
+
+void cursor_up(void)
+{
+	uint16_t offset = gap_start - current_line;
+
+	while (gap_start != current_line)
+		*--gap_end = *--gap_start;
+	if (gap_start == buffer_start)
+		return;
+
+	do
+		*--gap_end = *--gap_start;
+	while ((gap_start != buffer_start) && (gap_start[-1] != '\n'));
+
+	current_line = gap_start;
+	while (offset--)
+		cursor_right();
+}
+
+void goto_line(int lineno)
+{
+	while (gap_start != buffer_start)
+		*--gap_end = *--gap_start;
+	current_line = buffer_start;
+}
+
+const char editor_keys[] = "hjkl";
 void (*const editor_cb[])(void) =
 {
+	cursor_left,
+	cursor_down,
+	cursor_up,
 	cursor_right,
 };
 
@@ -281,14 +328,14 @@ void insert_file(const char* filename)
 
 	for (;;)
 	{
-		const uc* inptr;
+		const uint8_t* inptr;
 		if (read(fd, &cpm_default_dma, 128) != 128)
 			break;
 
 		inptr = cpm_default_dma;
 		while (inptr != (cpm_default_dma+128))
 		{
-			uc c = *inptr++;
+			uint8_t c = *inptr++;
 			if (c == 26) /* EOF */
 				break;
 			if (c != '\r')
@@ -302,17 +349,18 @@ void insert_file(const char* filename)
 			}
 		}
 	}
-	
+
 	close(fd);
 }
 
 void main(int argc, const char* argv[])
 {
 	cpm_overwrite_ccp();
-	con_init();
+	con_clear();
 
 	buffer_start = cpm_ram;
-	buffer_end = cpm_ramtop;
+	buffer_end = cpm_ramtop-1;
+	*buffer_end = '\n';
 	cpm_ram = buffer_start;
 
 	itoa((uint16_t)(buffer_end - buffer_start), (char*)cpm_default_dma, 10);
@@ -321,23 +369,33 @@ void main(int argc, const char* argv[])
 
 	new_file();
 	insert_file("ccp.asm");
+	goto_line(0);
 
+	con_goto(0, 0);
+	render_screen(first_line);
+	old_current_line = current_line;
 	for (;;)
 	{
-		char c;
-		char* cmdptr;
-		render_screen();
-		for (;;) {
-		c = bios_conin();
-		con_putc(c);
-		con_refresh();
-		}
+		const char* cmdp;
+		uint16_t length;
 
 		#if 0
-		cmdptr = strchr(editor_keys, c);
-		if (cmdptr)
-			editor_cb[cmdptr - editor_keys]();
+		itoa((uint16_t)(gap_start - buffer_start), (char*)cpm_default_dma, 10);
+		strcat((char*)cpm_default_dma, " ");
+		itoa((uint16_t)(buffer_end - gap_end), (char*)cpm_default_dma + strlen(cpm_default_dma), 10);
+		set_status_line((char*) cpm_default_dma);
 		#endif
+
+		if (current_line != old_current_line)
+			recompute_screen_position();
+		old_current_line = current_line;
+
+		length = compute_length(current_line, gap_start, NULL);
+		con_goto(length % WIDTH, current_line_y + (length / WIDTH));
+
+		cmdp = strchr(editor_keys, bios_conin());
+		if (cmdp)
+			editor_cb[cmdp - editor_keys]();
 	}
 }
 
