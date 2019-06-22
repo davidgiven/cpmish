fd825c3d6f06edff8588cb2287dae31efb45fa73
David Given <dg@cowlark.com>
2019-06-22 15:32:05 +0200

    Initial screen rendering works.

--- cpmtools/qe.c
+++ cpmtools/qe.c
@@ -3,6 +3,8 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
+#include <sys/types.h>
+#include <fcntl.h>
 #include <cpm.h>
 
 #define WIDTH 80
@@ -15,13 +17,28 @@ uc logical_screen[HEIGHT][WIDTH];
 #define logical_screen_start (logical_screen[0])
 #define logical_screen_end (logical_screen[HEIGHT])
 uc cursorx, cursory;
-
 uc status_line_length;
+
+uc* buffer_start;
+uc* gap_start;
+uc* gap_end;
+uc* buffer_end;
+
+uc* first_line;
+uc* cursor;
+uc* current_line;
+uc current_line_y;
+
+void con_clear(void)
+{
+	memset(logical_screen, ' ', sizeof(logical_screen));
+	cursorx = cursory = 0;
+}
 
 void con_init(void)
 {
+	con_clear();
 	memset(physical_screen, ' ', sizeof(physical_screen));
-	memset(logical_screen, ' ', sizeof(logical_screen));
 	cpm_conout(26);
 }
 
@@ -54,7 +71,7 @@ void con_putc(uc c)
 		con_newline();
 }
 
-void con_puts(const uc* s)
+void con_puts(const char* s)
 {
 	for (;;)
 	{
@@ -142,20 +159,127 @@ void set_status_line(const char* message)
 	scr_goto(cursorx, cursory);
 }
 
+void new_file(void)
+{
+	gap_start = buffer_start;
+	gap_end = buffer_end;
+
+	first_line = buffer_start;
+	cursor = first_line;
+	current_line = first_line;
+	current_line_y = 0;
+}
+	
+void insert_file(const char* filename)
+{
+	int fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return;
+
+	for (;;)
+	{
+		const uc* inptr;
+		if (read(fd, &cpm_default_dma, 128) != 128)
+			break;
+
+		inptr = cpm_default_dma;
+		while (inptr != (cpm_default_dma+128))
+		{
+			uc c = *inptr++;
+			if (c == 26) /* EOF */
+				break;
+			if (c != '\r')
+			{
+				if (gap_start == gap_end)
+				{
+					set_status_line("Out of memory");
+					break;
+				}
+				*gap_start++ = c;
+			}
+		}
+	}
+	
+	close(fd);
+}
+
+void render_screen(void)
+{
+	uc* inp = first_line;
+	uc screenx = 0;
+	uc screeny = 0;
+	uc xo = 0;
+
+	con_clear();
+	while (cursory != HEIGHT)
+	{
+		uc c;
+
+		if (inp == gap_start)
+			inp = gap_end;
+		if (inp == buffer_end)
+		{
+			con_newline();
+			con_puts("<<EOF>>");
+			break;
+		}
+
+		if (inp == cursor)
+		{
+			screenx = cursorx;
+			screeny = cursory;
+		}
+		c = *inp++;
+		if (c == '\n')
+		{
+			con_newline();
+			xo = 0;
+
+			if (inp == current_line)
+				current_line_y = cursory;
+		}
+		else if (c == '\t')
+		{
+			do
+			{
+				con_putc(' ');
+				xo++;
+			}
+			while (xo & 7);
+		}
+		else
+		{
+			con_putc(c);
+			xo++;
+		}
+	}
+
+	con_goto(screenx, screeny);
+	con_refresh();
+}
+
 void main(int argc, const char* argv[])
 {
-	uc i;
-
+	cpm_overwrite_ccp();
 	con_init();
-	for (i=0; i<10; i++)
+
+	buffer_start = cpm_ram;
+	buffer_end = cpm_ramtop;
+	cpm_ram = buffer_start;
+
+	itoa((uint16_t)(buffer_end - buffer_start), cpm_default_dma, 10);
+	strcat(cpm_default_dma, " bytes free");
+	set_status_line(cpm_default_dma);
+
+	new_file();
+	insert_file("ccp.asm");
+
+	for (;;)
 	{
-		con_goto(70, i);
-		con_puts("This is some text");
+		uc c;
+		render_screen();
+		c = cpm_conin();
+		break;
 	}
-	con_goto(0, HEIGHT-1);
-	con_puts("This is the last line");
-	con_refresh();
-	set_status_line("This is status!");
-	cpm_conin();
 }
 
