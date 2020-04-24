458af1ecb1512a8b463e1f5e36a81f1bd0c20276
David Given <dg@cowlark.com>
2020-04-24 23:20:57 +0200

    Rework things so that the memory layout is defined in one place, and move the BIOS out of line with the rest of the startup image so we can extend it into track 0.

--- arch/wp2450ds/build.lua
+++ arch/wp2450ds/build.lua
@@ -2,6 +2,36 @@ include "third_party/ld80/build.lua"
 include "third_party/zmac/build.lua"
 include "utils/build.lua"
 
+--- Memory layout configuration ---------------------------------------------
+
+-- Configure the BIOS size here; this will then emit an addresses.lib file
+-- which contains the position of the BDOS and CCP.
+
+local BIOS_SIZE = 0x0e00
+local BDOS_SIZE = 3584            -- fixed
+local CCP_SIZE = 2048             -- fixed
+local BBASE = 0x10000 - BIOS_SIZE
+local FBASE = BBASE - BDOS_SIZE
+local CBASE = FBASE - CCP_SIZE
+
+normalrule {
+	name = "addresses_lib",
+	ins = {},
+	outleaves = { "addresses.lib" },
+	commands = {
+		"echo BIOS_SIZE = "..BIOS_SIZE.." > %{outs[1]}",
+		"echo BDOS_SIZE = "..BDOS_SIZE.." >> %{outs[1]}",
+		"echo CCP_SIZE = "..CCP_SIZE.." >> %{outs[1]}",
+		"echo BBASE = "..BBASE.." >> %{outs[1]}",
+		"echo FBASE = "..FBASE.." >> %{outs[1]}",
+		"echo CBASE = "..CBASE.." >> %{outs[1]}",
+	}
+}
+
+--- Generated tables --------------------------------------------------------
+
+-- Font and keyboard table.
+
 normalrule {
     name = "font_inc",
     ins = {
@@ -22,6 +52,10 @@ normalrule {
         "%{ins} > %{outs}"
     }
 }
+
+--- Libraries ---------------------------------------------------------------
+
+-- These may be used by both the BIOS proper and the bootstrap program.
 
 zmac {
     name = "tty_o",
@@ -55,33 +89,11 @@ zmac {
     },
 }
 
-zmac {
-    name = "bios_o",
-    srcs = { "./bios.z80" },
-    deps = {
-        "include/*.lib",
-        "./include/*.lib",
-		"arch/common/utils/tty.lib",
-		"arch/common/utils/upd765.lib",
-		"arch/common/utils/deblocker.lib",
-		"+font_inc"
-    },
-}
+--- Bootstrapper ------------------------------------------------------------
 
--- This is the bit which CP/M reloads on warm boot (well, some of it).
-ld80 {
-	name = "cpmfile_img",
-	address = 0xdc00,
-	srcs = {
-		"-Pdc00", "third_party/zcpr1+zcpr",
-		"-Pe400", "third_party/zsdos+zsdos",
-		"-Pf200",
-		"+bios_o",
-		"+upd765_o",
-		"+tty_o",
-		"+keyboard_o",
-	}
-}
+-- This is the .APL file which the Brother OS loads. It's responsible for
+-- remapping the memory, doing some device setup, and loading the BIOS into
+-- the top of memory.
 
 zmac {
     name = "boot_o",
@@ -91,6 +103,7 @@ zmac {
         "./include/*.lib",
 		"arch/common/utils/upd765.lib",
 		"arch/common/utils/deblocker.lib",
+		"+addresses_lib"
     },
 }
 
@@ -104,20 +117,75 @@ ld80 {
     }
 }
 
--- Produces the FAT bit of the disk image.
+--- BIOS --------------------------------------------------------------------
+
+-- The CP/M BIOS itself.
+
 zmac {
-	name = "fat_o",
-	srcs = { "./fat.z80" },
-	deps = {
-		"+boot_img",
-		"+cpmfile_img"
+    name = "bios_o",
+    srcs = { "./bios.z80" },
+    deps = {
+        "include/*.lib",
+        "./include/*.lib",
+		"arch/common/utils/tty.lib",
+		"arch/common/utils/upd765.lib",
+		"arch/common/utils/deblocker.lib",
+		"+font_inc",
+		"+addresses_lib",
+    },
+}
+
+-- This is a 64kB file containing the entire CP/M memory image.
+
+ld80 {
+	name = "memory_img",
+	address = 0,
+	srcs = {
+		"-P"..string.format("%x", CBASE), "third_party/zcpr1+zcpr",
+		"-P"..string.format("%x", FBASE), "third_party/zsdos+zsdos",
+		"-P"..string.format("%x", BBASE),
+		"+bios_o",
+		"+upd765_o",
+		"+tty_o",
+		"+keyboard_o",
 	}
 }
 
-ld80 {
-	name = "bootfile_img",
-	srcs = { "+fat_o" }
+-- We now chop it up to remove the BIOS and the combined CCP/BDOS (which
+-- go into the system track).
+
+binslice {
+	name = "bios_img",
+	src = { "+memory_img" },
+	start = BBASE,
+	length = BIOS_SIZE
 }
+
+binslice {
+	name = "systemtrack_img",
+	src = { "+memory_img" },
+	start = CBASE,
+	length = BBASE - CBASE
+}
+
+--- FAT file system ---------------------------------------------------------
+
+-- Produces the FAT bit of the disk image.
+
+zmac {
+	name = "bootfile_img",
+	srcs = { "./fat.z80" },
+	deps = {
+		"+boot_img",
+		"+bios_img",
+		"+systemtrack_img"
+	},
+	relocatable = false
+}
+
+--- Disk image --------------------------------------------------------------
+
+-- Assembles the bootable disk which you can actually run.
 
 unix2cpm {
     name = "readme",
